9227befe0b964fbd980bec481476d230af90e236
arunima-adhikary7 <aarunima790@gmail.com>
2026-08-24 21:02:54 +0530

    add

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ["http://localhost:5173", "https://your-portfolio.vercel.app"]
+  origin: ["http://localhost:5173", "https://client-one-psi-91.vercel.app/"]
 }));
 
 app.use(express.json());
